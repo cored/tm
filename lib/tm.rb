dbb7fe61b9b14bea8baf1ca94c74964cf5df0bdd
Rafael George <george.rafael@gmail.com>
2013-03-27 14:23:39 +0000

    Refactor: Remove code for handling taskmapper from global module

--- lib/tm.rb
+++ lib/tm.rb
@@ -1,32 +1,20 @@
 require 'tm/version.rb'
-require 'taskmapper'
-require 'taskmapper-kanbanpad'
+require 'tm/provider_handler'
+require 'tm/exceptions'
 
 module TM
 
-  def list_projects(provider, authentication)
-    tm = TaskMapper.new(provider, format_authentication(authentication))
-    print_projects(tm.projects)
+  def list_projects(provider_handler)
+    print_projects(provider_handler.projects)
   rescue NameError => ex
     raise InvalidProvider, "Provider doesn't exist"    
   end
 
   private
-  def format_authentication(authentication)
-    res = authentication.split(/,/).inject({}) do |res, kv|
-      arg, val = kv.split(/:/)
-      res[arg] = val
-      res
-    end
-    res
-  end
-
   def print_projects(projects)
     projects.each do |project|
       printf("%s, %s, %s, %s \n", project.id, project.name, project.created_at, project.updated_at)
     end
   end
 
-  class Error < StandardError; end
-  class InvalidProvider < Error; end
 end
